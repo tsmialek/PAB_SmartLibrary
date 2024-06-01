25c14a82e6ece23a224b75146e5ad72abb1e3cd3
tsmialek <tsmialek.1@gmail.com>
2024-06-01 18:21:02 +0200

    feat: AddAdminUserSeedData migration.

---
+++ SmartLibrary.Infrastructure/Migrations/20240601155911_AddAdminUserSeedData.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace SmartLibrary.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdminUserSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRoles_Roles_RolesId",
+                table: "UserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRoles_Users_UsersId",
+                table: "UserRoles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserRoles",
+                table: "UserRoles");
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("101d3efd-9704-42f0-b788-113d5689a238"));
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("457d6a44-d983-4940-a9fc-fe0bbbf1cf93"));
+
+            migrationBuilder.RenameTable(
+                name: "UserRoles",
+                newName: "RoleUser");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserRoles_UsersId",
+                table: "RoleUser",
+                newName: "IX_RoleUser_UsersId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RoleUser",
+                table: "RoleUser",
+                columns: new[] { "RolesId", "UsersId" });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("1a6590fc-0644-40f2-9ea4-939fafc3e3eb"), "Admin" },
+                    { new Guid("1ead1b7c-f356-4911-8f78-9ccaab78f0b0"), "User" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "FirstName", "LastName", "Password" },
+                values: new object[] { new Guid("4b06c5f7-e899-425d-878c-9173ad44d65e"), "admin@wsei.pl", "Tomasz", "Smialek", "admin" });
+
+            migrationBuilder.InsertData(
+                table: "RoleUser",
+                columns: new[] { "RolesId", "UsersId" },
+                values: new object[] { new Guid("1a6590fc-0644-40f2-9ea4-939fafc3e3eb"), new Guid("4b06c5f7-e899-425d-878c-9173ad44d65e") });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RoleUser_Roles_RolesId",
+                table: "RoleUser",
+                column: "RolesId",
+                principalTable: "Roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RoleUser_Users_UsersId",
+                table: "RoleUser",
+                column: "UsersId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RoleUser_Roles_RolesId",
+                table: "RoleUser");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RoleUser_Users_UsersId",
+                table: "RoleUser");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RoleUser",
+                table: "RoleUser");
+
+            migrationBuilder.DeleteData(
+                table: "RoleUser",
+                keyColumns: new[] { "RolesId", "UsersId" },
+                keyValues: new object[] { new Guid("1a6590fc-0644-40f2-9ea4-939fafc3e3eb"), new Guid("4b06c5f7-e899-425d-878c-9173ad44d65e") });
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("1ead1b7c-f356-4911-8f78-9ccaab78f0b0"));
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("1a6590fc-0644-40f2-9ea4-939fafc3e3eb"));
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("4b06c5f7-e899-425d-878c-9173ad44d65e"));
+
+            migrationBuilder.RenameTable(
+                name: "RoleUser",
+                newName: "UserRoles");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RoleUser_UsersId",
+                table: "UserRoles",
+                newName: "IX_UserRoles_UsersId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserRoles",
+                table: "UserRoles",
+                columns: new[] { "RolesId", "UsersId" });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("101d3efd-9704-42f0-b788-113d5689a238"), "User" },
+                    { new Guid("457d6a44-d983-4940-a9fc-fe0bbbf1cf93"), "Admin" }
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRoles_Roles_RolesId",
+                table: "UserRoles",
+                column: "RolesId",
+                principalTable: "Roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRoles_Users_UsersId",
+                table: "UserRoles",
+                column: "UsersId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
