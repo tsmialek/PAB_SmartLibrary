93261d490a84c1dca6a3e04724dbacaa2463ebd0
tsmialek <tsmialek.1@gmail.com>
2024-06-04 22:14:29 +0200

    feat(tests): Add unit test project, and unit tests for AuthenticationService and BookService.

---
+++ SmartLibrary.Tests/Services/AuthenticationServiceTests.cs
@@ -0,0 +1,110 @@
+﻿using FluentAssertions;
+using Moq;
+using SmartLibrary.Application.Common.Error.Authentication;
+using SmartLibrary.Application.Common.Interfaces.Authentication;
+using SmartLibrary.Application.Common.Interfaces.Persistance;
+using SmartLibrary.Application.Services.Authentication;
+using SmartLibrary.Domain.Entities;
+
+namespace SmartLibrary.Tests.Services
+{
+    public class AuthenticationServiceTests
+    {
+        private readonly Mock<IJwtTokenGenerator> _jwtTokenGeneratorMock;
+        private readonly Mock<IUserRepository> _userRepositoryMock;
+        private readonly Mock<IRoleRepository> _roleRepositoryMock;
+        private readonly AuthenticationService _authenticationService;
+
+        public AuthenticationServiceTests()
+        {
+            _jwtTokenGeneratorMock = new Mock<IJwtTokenGenerator>();
+            _userRepositoryMock = new Mock<IUserRepository>();
+            _roleRepositoryMock = new Mock<IRoleRepository>();
+            _authenticationService = new AuthenticationService(_jwtTokenGeneratorMock.Object, _userRepositoryMock.Object, _roleRepositoryMock.Object);
+        }
+
+        [Fact]
+        public void Register_WithExistingEmail_ShouldThrowDuplicateEmailException()
+        {
+            // Arrange
+            var email = "test@example.com";
+            var existingUser = new User { Email = email };
+            _userRepositoryMock.Setup(repo => repo.GetUserByEmail(email)).Returns(existingUser);
+
+            // Act
+            var act = () => _authenticationService.Register("FirstName", "LastName", email, "Password");
+
+            // Assert
+            act.Should().Throw<DuplicateEmailException>();  
+        }
+
+        [Fact]
+        public void Register_WithNewEmail_ShouldReturnAuthenticationResult()
+        {
+            // Arrange 
+            var email = "newUser@example.com";
+            _userRepositoryMock.Setup(repo => repo.GetUserByEmail(email)).Returns((User)null);
+            var newUser = new User { Email = email };
+            _userRepositoryMock.Setup(repo => repo.Add(It.IsAny<User>())).Callback<User>(user => newUser = user);
+            var token = "jwt_token";
+            _jwtTokenGeneratorMock.Setup(jwt => jwt.GenerateToken(It.IsAny<User>())).Returns(token);
+
+            // Act
+            var result = _authenticationService.Register("FirstName", "LastName", email, "Password");
+
+            // Assert 
+            result.Should().NotBeNull();
+            result.Token.Should().Be(token);
+            result.User.Should().Be(newUser);
+        }
+
+        [Fact]
+        public void Login_WithInvalidEmail_ShouldThrowNonExistingUserException()
+        {
+            // Arrange
+            var email = "nonExistingUser@example.com";
+            _userRepositoryMock.Setup(repo => repo.GetUserByEmail(email)).Returns((User)null);
+
+            // Act
+            var result = () => _authenticationService.Login(email, "Password");
+
+            // Assert
+            result.Should().Throw<NonExistingUserException>();
+        }
+
+        [Fact]
+        public void Login_WithInvalidPassword_ShoudThrowInvalidCredentialsException()
+        {
+            // Arrange
+            var email = "user@example.com";
+            var user = new User { Email = email, Password = "correctPassword" };
+            _userRepositoryMock.Setup(repo => repo.GetUserByEmail(email)).Returns(user);
+
+            // Act
+            var result = () => _authenticationService.Login(email, "Password");
+
+            // Assert
+            result.Should().Throw<InvalidCredentialsException>();
+        }
+
+        [Fact]
+        public void Login_WithValidCredentials_ShouldReturnAuthenticationResult()
+        {
+            // Arrange 
+            var email = "user@example.com";
+            var password = "correctPassword";
+            var user = new User { Email = email, Password = password };
+            _userRepositoryMock.Setup(repo => repo.GetUserByEmail(email)).Returns(user);
+            var token = "jwt_token";
+            _jwtTokenGeneratorMock.Setup(jwt => jwt.GenerateToken(user)).Returns(token);
+
+            // Act
+            var result = _authenticationService.Login(email, password);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.User.Should().Be(user);
+            result.Token.Should().Be(token);
+        }
+    }
+}
---
+++ SmartLibrary.Tests/Services/BookServiceTests.cs
@@ -0,0 +1,158 @@
+﻿using FluentAssertions;
+using Moq;
+using SmartLibrary.Application.Common.Error.Book;
+using SmartLibrary.Application.Common.Interfaces.Persistance;
+using SmartLibrary.Application.Services.BookMenagement;
+using SmartLibrary.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartLibrary.Tests.Services
+{
+    public class BookServiceTests
+    {
+        private readonly Mock<IBookRepository> _bookRepositoryMock;
+        private readonly BookService _bookService;
+
+        public BookServiceTests()
+        {
+            _bookRepositoryMock = new Mock<IBookRepository>();
+            _bookService = new BookService(_bookRepositoryMock.Object);
+        }
+
+        [Fact]
+        public void ListBook_ShouldReturnListOfBooks()
+        {
+            // Arrange
+            var books = new List<Book>
+            {
+                new Book { Id = Guid.NewGuid(), Title = "Book1", Author = "Author1", ISBN = "ISBN1" },
+                new Book { Id = Guid.NewGuid(), Title = "Book2", Author = "Author2", ISBN = "ISBN2" },
+                new Book { Id = Guid.NewGuid(), Title = "Book3", Author = "Author3", ISBN = "ISBN3" }
+            };
+            _bookRepositoryMock.Setup(repo => repo.GetAll()).Returns(books);
+
+            // Act 
+            var result = _bookService.GetBooks();
+
+            // Assert 
+            result.Should().NotBeNull();
+            result.Should().BeEquivalentTo(books);
+        }
+
+        [Fact]
+        public void GetBookById_WithExistingId_ShouldReturnBook()
+        {
+            // Arrange
+            var bookId = Guid.NewGuid();
+            var book = new Book { Id = bookId, Author = "Author", Title = "Title", ISBN = "ISBN" };
+            _bookRepositoryMock.Setup(repo => repo.GetById(bookId)).Returns(book);
+
+            // Act 
+            var result = _bookService.GetBookById(bookId);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Book.Should().Be(book);
+        }
+
+        [Fact]
+        public void GetBookById_WithNonExistingId_ShouldThrowBookNotFoundException()
+        {
+            // Arrange
+            var nonExistingBookId = Guid.NewGuid();
+            var book = new Book { Id = Guid.NewGuid(), Author = "Author", Title = "Title", ISBN = "ISBN" };
+            _bookRepositoryMock.Setup(repo => repo.GetById(nonExistingBookId)).Returns((Book)null);
+
+            // Act
+            var result = () => _bookService.GetBookById(nonExistingBookId);
+
+            // Assert
+            result.Should().Throw<BookNotFoundException>();
+        }
+
+        [Fact]
+        public void GetBookByName_WithExistingName_ShouldReturnBook()
+        {
+            // Arrange
+            var bookName = "existingBookName";
+            var book = new Book { Author = "Author", Title = bookName, ISBN = "ISBN" };
+            _bookRepositoryMock.Setup(repo => repo.GetByName(bookName)).Returns(book);
+
+            // Act 
+            var result = _bookService.GetBookByName(bookName);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Book.Should().Be(book);
+        }
+
+        [Fact]
+        public void GetBookByName_WithNonExistingName_ShouldThrowBookNotFoundException()
+        {
+            // Arrange
+            var nonExistingBookName = "nonExistingBookName";
+            var book = new Book { Author = "Author", Title = "existingBookName", ISBN = "ISBN" };
+            _bookRepositoryMock.Setup(repo => repo.GetByName(nonExistingBookName)).Returns((Book)null);
+
+            // Act
+            var result = () => _bookService.GetBookByName(nonExistingBookName);
+
+            // Assert
+            result.Should().Throw<BookNotFoundException>();
+        }
+
+        [Fact]
+        public void AddBook_WithValidBook_ShouldReturnAddedBookResult()
+        {
+            // Arrange
+            var book = new Book { Author = "Author", Title = "Title", ISBN = "ISBN" };
+            _bookRepositoryMock.Setup(repo => repo.Add(It.IsAny<Book>())).Callback<Book>(b => b.Id = Guid.NewGuid());
+
+            // Act 
+            var result = _bookService.AddBook(book.Title, book.Author, book.ISBN);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Book.Should().NotBeNull();
+            result.Book.Id.Should().NotBe(Guid.Empty);
+            result.Book.Title.Should().Be(book.Title);
+            result.Book.Author.Should().Be(book.Author);
+            result.Book.ISBN.Should().Be(book.ISBN);
+        }
+
+        [Fact]
+        public void DeleteBook_WithValidId_ShouldReturnDeletedBookResult()
+        {
+            // Arrange
+            var bookId = Guid.NewGuid();
+            var book = new Book { Id = bookId, Title = "Test Book", Author = "Author", ISBN = "ISBN" };
+            _bookRepositoryMock.Setup(repo => repo.GetById(bookId)).Returns(book);
+            _bookRepositoryMock.Setup(repo => repo.Delete(bookId));
+
+            // Act
+            var result = _bookService.DeleteBook(bookId);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Book.Should().Be(book);
+        }
+
+        [Fact]
+        public void DeleteBook_WithInvalidId_ShouldThrowBookNotFoundException()
+        {
+            // Arrange
+            var bookId = Guid.NewGuid();
+            _bookRepositoryMock.Setup(repo => repo.GetById(bookId)).Returns((Book)null);
+
+            // Act
+            var act = () => _bookService.DeleteBook(bookId);
+
+            // Assert
+            act.Should().Throw<BookNotFoundException>();
+        }
+    }
+}
