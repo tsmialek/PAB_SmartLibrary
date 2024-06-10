cb1cd5cd18c7fe82b49f82f6394d232d3ebed498
tsmialek <tsmialek.1@gmail.com>
2024-06-10 20:30:22 +0200

    feat(RazorPagesAdmin): Add Navigation. Add BooksManagement, UserManagement and RoleManagement Pages that consume api.
    Code is ugly, needs to be refactored but at least it works.

--- SmartLibrary.RazorPagesAdmin/Pages/Admin/RoleManagement.cshtml.cs
+++ SmartLibrary.RazorPagesAdmin/Pages/Admin/RoleManagement.cshtml.cs
@@ -1,12 +1,166 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using SmartLibrary.RazorPagesAdmin.Models;
+using System.Net.Http.Headers;
+using System.Text;
 
 namespace SmartLibrary.RazorPagesAdmin.Pages.Admin
 {
     public class RoleManagementModel : PageModel
     {
-        public void OnGet()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public RoleManagementModel(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public List<string> UserRoles { get; private set; }
+        public string UserEmail { get; private set; }
+
+        public List<RoleResponse> Roles { get; private set; } = new List<RoleResponse>();
+
+        [BindProperty]
+        public string RoleName { get; set; }
+
+        [BindProperty]
+        public AddUserRoleByEmailAndNameRequest AddUserRoleByEmailRequest { get; set; }
+
+        [BindProperty]
+        public AddUserRoleByIdRequest AddUserRoleById { get; set; }
+
+        [TempData]
+        public string ErrorMessage { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString("UserEmail")))
+            {
+                return RedirectToPage("/Login");
+            }
+
+            UserEmail = HttpContext.Session.GetString("UserEmail");
+
+            var rolesJson = HttpContext.Session.GetString("UserRoles");
+            UserRoles = rolesJson != null ? JsonConvert.DeserializeObject<List<string>>(rolesJson) : new List<string>();
+
+            await LoadRolesAsync();
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAddUserRoleByEmailAsync()
+        {
+            var client = _httpClientFactory.CreateClient("SmartLibraryAPI");
+            var accessToken = HttpContext.Session.GetString("Token");
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var content = new StringContent(JsonConvert.SerializeObject(AddUserRoleByEmailRequest), Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:7207/roles/addUserRoleByEmail", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                await LoadRolesAsync();
+            }
+            else
+            {
+                await HandleErrorResponse(response);
+            }
+
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostAddUserRoleByIdAsync()
+        {
+            var client = _httpClientFactory.CreateClient("SmartLibraryAPI");
+            var accessToken = HttpContext.Session.GetString("Token");
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var content = new StringContent(JsonConvert.SerializeObject(AddUserRoleById), Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:7207/roles/addUserRoleById", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                await LoadRolesAsync();
+            }
+            else
+            {
+                await HandleErrorResponse(response);
+            }
+
+            return RedirectToPage();
+        }
+
+        private async Task LoadRolesAsync()
+        {
+            var client = _httpClientFactory.CreateClient("SmartLibraryAPI");
+            var accessToken = HttpContext.Session.GetString("Token");
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var response = await client.GetAsync("https://localhost:7207/roles");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseString = await response.Content.ReadAsStringAsync();
+                Roles = JsonConvert.DeserializeObject<List<RoleResponse>>(responseString);
+            }
+            else
+            {
+                await HandleErrorResponse(response);
+            }
+        }
+
+        private async Task HandleErrorResponse(HttpResponseMessage response)
+        {
+            var responseString = await response.Content.ReadAsStringAsync();
+            var errorResponse = JsonConvert.DeserializeObject<ErrorResponse>(responseString);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Forbidden)
+            {
+                ErrorMessage = "You don't have permissions to access this endpoint";
+            }
+            else
+            {
+                ErrorMessage = errorResponse.Title;
+            }
+        }
+
+        public IActionResult OnPostLogout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToPage("/Login");
+        }
+
+        public class RoleResponse
+        {
+            public Guid Id { get; set; }
+            public string Name { get; set; }
+            public List<User> Users { get; set; }
+        }
+
+        public class User
+        {
+            public Guid Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class AddUserRoleByEmailAndNameRequest
+        {
+            public string UserEmail { get; set; }
+            public string NewUserRoleName { get; set; }
+        }
+
+        public class AddUserRoleByIdRequest
+        {
+            public Guid UserId { get; set; }
+            public Guid NewUserRoleId { get; set; }
+        }
+
+        public class ErrorResponse
+        {
+            public string Title { get; set; }
         }
     }
 }
--- SmartLibrary.RazorPagesAdmin/Pages/Admin/UserManagement.cshtml.cs
+++ SmartLibrary.RazorPagesAdmin/Pages/Admin/UserManagement.cshtml.cs
@@ -1,12 +1,197 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using SmartLibrary.RazorPagesAdmin.Models;
+using System.Net.Http.Headers;
+using System.Text;
 
 namespace SmartLibrary.RazorPagesAdmin.Pages.Admin
 {
     public class UserManagementModel : PageModel
     {
-        public void OnGet()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public string Token { get; private set; }
+        public List<string> UserRoles { get; private set; }
+        public string UserEmail { get; private set; }
+
+        public UserManagementModel(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public List<UserResponse> Users { get; private set; } = new List<UserResponse>();
+
+        [BindProperty]
+        public RegisterRequest Input { get; set; }
+
+        [BindProperty]
+        public string SearchEmail { get; set; }
+
+        [TempData]
+        public string ErrorMessage { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString("UserEmail")))
+            {
+                return RedirectToPage("/Login");
+            }
+
+            var client = _httpClientFactory.CreateClient("SmartLibraryAPI");
+            var accessToken = HttpContext.Session.GetString("Token");
+
+            UserEmail = HttpContext.Session.GetString("UserEmail");
+            var rolesJson = HttpContext.Session.GetString("UserRoles");
+            UserRoles = rolesJson != null ? JsonConvert.DeserializeObject<List<string>>(rolesJson) : new List<string>();
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var response = await client.GetAsync("https://localhost:7207/auth/all");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseString = await response.Content.ReadAsStringAsync();
+                Users = JsonConvert.DeserializeObject<List<UserResponse>>(responseString);
+            }
+            else
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.Forbidden)
+                {
+                    ErrorMessage = "You don't have permissions to access this endpoint";
+                }
+                else
+                {
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    var errorResponse = JsonConvert.DeserializeObject<ErrorResponse>(responseString);
+                    ErrorMessage = errorResponse.Title;
+                }
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostRegisterUserAsync()
+        {
+            var client = _httpClientFactory.CreateClient("SmartLibraryAPI");
+            var accessToken = HttpContext.Session.GetString("Token");
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var content = new StringContent(JsonConvert.SerializeObject(Input), Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:7207/auth/register", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                await LoadUsersAsync();
+            }
+            else
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.Forbidden)
+                {
+                    ErrorMessage = "You don't have permissions to access this endpoint";
+                }
+                else
+                {
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    var errorResponse = JsonConvert.DeserializeObject<ErrorResponse>(responseString);
+                    ErrorMessage = errorResponse.Title;
+                }
+            }
+
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostSearchUserByEmailAsync()
+        {
+            var client = _httpClientFactory.CreateClient("SmartLibraryAPI");
+            var accessToken = HttpContext.Session.GetString("Token");
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var response = await client.GetAsync($"https://localhost:7207/auth/email/{SearchEmail}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseString = await response.Content.ReadAsStringAsync();
+                var user = JsonConvert.DeserializeObject<UserResponse>(responseString);
+                Users = [user];
+            }
+            else
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.Forbidden)
+                {
+                    ErrorMessage = "You don't have permissions to access this endpoint";
+                }
+                else
+                {
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    var errorResponse = JsonConvert.DeserializeObject<ErrorResponse>(responseString);
+                    ErrorMessage = errorResponse.Title;
+                }
+            }
+
+            return Page();
+        }
+
+        private async Task LoadUsersAsync()
+        {
+            var client = _httpClientFactory.CreateClient("SmartLibraryAPI");
+            var accessToken = HttpContext.Session.GetString("Token");
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var response = await client.GetAsync("https://localhost:7207/auth/all");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseString = await response.Content.ReadAsStringAsync();
+                Users = JsonConvert.DeserializeObject<List<UserResponse>>(responseString);
+            }
+            else
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.Forbidden)
+                {
+                    ErrorMessage = "You don't have permissions to access this endpoint";
+                }
+                else
+                {
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    var errorResponse = JsonConvert.DeserializeObject<ErrorResponse>(responseString);
+                    ErrorMessage = errorResponse.Title;
+                }
+            }
+        }
+
+        public IActionResult OnPostLogout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToPage("/Login");
+        }
+
+        public class RegisterRequest
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class UserResponse
+        {
+            public Guid Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+            public List<Role> Roles { get; set; }
+        }
+
+        public class Role
+        {
+            public Guid Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class ErrorResponse
+        {
+            public string Title { get; set; }
         }
     }
 }
